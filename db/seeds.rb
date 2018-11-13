2475c1effde0462c23272679e69946d2ee88144f
Lisa Conti <leconti2@gmail.com>
2018-11-13 11:25:36 +1100

    created an idex page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Product.create!(
+  name: "Living Proof Shampoo",
+  description: "Wonderful shampoo",
+  price: 17,
+  user_id: 1
+)
