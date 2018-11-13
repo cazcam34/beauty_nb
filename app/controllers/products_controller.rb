fe4d945a47e6ba146a34d06dcfc170d0bf19bfa9
Caroline Cameron <carolinecameron_34@hotmail.com>
2018-11-13 11:17:29 +1100

    added list product feature and fixed up some bugs from master

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,17 @@
 class ProductsController < ApplicationController
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    @product.user_id = current_user.id
+    @product.save
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :description, :price)
+  end
 end
