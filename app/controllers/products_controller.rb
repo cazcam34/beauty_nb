19ba694a9eaac1f833ef71c06e95093decdb86b4
Lisa Conti <leconti2@gmail.com>
2018-11-13 13:59:26 +1100

    added search bar to index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,18 @@
 class ProductsController < ApplicationController
+
   def index
     @products = Product.all
+    if params[:query].present?
+      sql_query = "name ILIKE :query OR description ILIKE :query"
+      @products = Product.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @products = Product.all
+    end
   end
-  
-	def show
-		@product = Product.find(params[:id])
-	end
+
+  def show
+    @product = Product.find(params[:id])
+  end
 
   def new
     @product = Product.new
@@ -16,11 +23,10 @@ class ProductsController < ApplicationController
     @product.user_id = current_user.id
     @product.save
   end
-  
+
   private
 
   def product_params
     params.require(:product).permit(:name, :description, :price)
   end
 end
-
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,7 @@
 import "bootstrap";
+
+import { initSearchOnKeyUp } from '../components/search';
+initSearchOnKeyUp();
+
+import { clearFormAfterEnter } from '../components/search';
+clearFormAfterEnter();
