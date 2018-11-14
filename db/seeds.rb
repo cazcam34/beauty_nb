ac44dd7edd0d6f350b71cf6e0f61c2aef2998f39
Jessie <jessie.baxter29@gmail.com>
2018-11-14 11:04:06 +1100

    Updated seed with beauty product API

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,11 @@ puts 'Creating products...'
 data = JSON.load(open("https://makeup-api.herokuapp.com/api/v1/products.json?product_type=foundation"))
 
 data[0...1].each do |product|
-	p product["name"]
-	p product["description"]
-	p product["price"].to_i
-	p product["image_link"]
-	# Product.create(name: product["name"], description: product["description"], price: product["price"].to_i, photo: product["image_link"], user_id: 1)
+	# p product["name"]
+	# p product["description"]
+	# p product["price"].to_i
+	# p product["image_link"]
+	Product.create(name: product["name"], description: product["description"], price: product["price"].to_i, photo: product["image_link"], user_id: 1)
 end
 
 puts 'Finished!'
