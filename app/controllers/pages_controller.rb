4e26af35a5a9bc86a632eeaee18ceed1de3727c9
Jessie <jessie.baxter29@gmail.com>
2018-11-15 13:55:20 +1100

    Added popular products to dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,16 +4,29 @@ class PagesController < ApplicationController
 
   def dashboard
   	favs
+  	popular
   end
 
   def favs
   	orders = Order.where(["user_id = ? and status = ?", current_user, "complete"])
- 		@products = []
+ 		products = []
  		orders.each do |order|
  			order.products.each do |product|
- 				@products << product
+ 				products << product
  			end
  		end
- 		@products.uniq!
+ 		@fav_products = products.uniq[0...6]
+  end
+
+  def popular
+  	products = Product.all
+ 		products_freq = Hash.new(0)
+ 		products.each do |product|
+ 			products_freq[product] = product.order_items.count
+ 		end
+ 		@popular_products = []
+ 		products_freq.sort_by { |_key, value| value }.reverse[0...6].each do |product|
+ 			@popular_products << product[0]
+ 		end
   end
 end
